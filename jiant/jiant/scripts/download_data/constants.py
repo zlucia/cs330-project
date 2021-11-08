cfc8a0b1b58601c34d54cff30a05ed843cc806f5
Lucia Zheng <zlucia@stanford.edu>
2021-11-08 00:12:38 -0800

    Fix set syntax

--- jiant/jiant/scripts/download_data/constants.py
+++ jiant/jiant/scripts/download_data/constants.py
@@ -64,9 +64,9 @@ base_hf_datasets_tasks = {
 }
 
 hendrycks_test_tasks = ['abstract_algebra', 'anatomy', 'astronomy', 'business_ethics', 'clinical_knowledge', 'college_biology', 'college_chemistry', 'college_computer_science', 'college_mathematics', 'college_medicine', 'college_physics', 'computer_security', 'conceptual_physics', 'econometrics', 'electrical_engineering', 'elementary_mathematics', 'formal_logic', 'global_facts', 'high_school_biology', 'high_school_chemistry', 'high_school_computer_science', 'high_school_european_history', 'high_school_geography', 'high_school_government_and_politics', 'high_school_macroeconomics', 'high_school_mathematics', 'high_school_microeconomics', 'high_school_physics', 'high_school_psychology', 'high_school_statistics', 'high_school_us_history', 'high_school_world_history', 'human_aging', 'human_sexuality', 'international_law', 'jurisprudence', 'logical_fallacies', 'machine_learning', 'management', 'marketing', 'medical_genetics', 'miscellaneous', 'moral_disputes', 'moral_scenarios', 'nutrition', 'philosophy', 'prehistory', 'professional_accounting', 'professional_law', 'professional_medicine', 'professional_psychology', 'public_relations', 'security_studies', 'sociology', 'us_foreign_policy', 'virology', 'world_religions']
-hendrycks_test_hf_datasets_tasks = {}
+hendrycks_test_hf_datasets_tasks = set()
 for task in hendrycks_test_tasks:
-    hendrycks_test_hf_datasets_tasks.update(task + "_val")
-    hendrycks_test_hf_datasets_tasks.update(task + "_test")
+    hendrycks_test_hf_datasets_tasks.add(task + "_val")
+    hendrycks_test_hf_datasets_tasks.add(task + "_test")
 
 OTHER_HF_DATASETS_TASKS = base_hf_datasets_tasks.union(hendrycks_test_hf_datasets_tasks)
